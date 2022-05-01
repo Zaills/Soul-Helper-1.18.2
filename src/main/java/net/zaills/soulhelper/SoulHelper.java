3a6369a0f81462b8e2fcdf469b06ec3766c78788
Zaills <39807152+Zaills@users.noreply.github.com>
2022-05-01 02:58:56 +0200

    add Soul Ingot, Soul nugget and Raw Soul

--- src/main/java/net/zaills/soulhelper/SoulHelper.java
+++ src/main/java/net/zaills/soulhelper/SoulHelper.java
@@ -1,21 +1,19 @@
 package net.zaills.soulhelper;
 
 import net.fabricmc.api.ModInitializer;
+import net.zaills.soulhelper.item.ModItems;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class SoulHelper implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+
+	public static final String MOD_ID = "soulhelper";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItem();
+
 	}
 }
---
+++ src/main/java/net/zaills/soulhelper/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.zaills.soulhelper.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.ItemGroup;
+import net.zaills.soulhelper.SoulHelper;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    public static final Item Soul_Ingot = registerItem("soul_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item Soul_Nugget = registerItem("soul_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item Soul_Raw = registerItem("soul_raw",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registry.ITEM, new Identifier(SoulHelper.MOD_ID, name), item);
+    }
+
+    public static void registerModItem(){
+        SoulHelper.LOGGER.info("Registering Mod Items for" + SoulHelper.MOD_ID);
+    }
+
+}
